885439979c5faa5b8975bcef7c03d1a0f7b85870
abodell <andybodell9@gmail.com>
2022-06-01 13:55:54 -0400

    Added cards to the display to show the info

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 const pages = document.querySelector('#pages');
 const isRead = document.querySelector('#read');
+const bookDisplay = document.querySelector('.book-display');
 
 function Book(title, author, pages, isRead) { // book object
     this.title = title;
@@ -19,7 +20,19 @@ Book.prototype.addToLibrary = function() {
     library.push(this);
 }
 
-Book.prototype.makeBook = () => {
+const book1 = new Book('Ashshs', 'ashhs', 200, true);
+const book2 = new Book('asjhsakh', 'ajskjsa', 300, true);
+const book3 = new Book('ajshkajs', 'asshasjh', 500, true);
+const book4 = new Book('ashjhsag', 'ashbx', 300, false);
+const book5 = new Book('asjsjs', 'akshsah', 800, false);
+
+book1.addToLibrary();
+book2.addToLibrary();
+book3.addToLibrary();
+book4.addToLibrary();
+book5.addToLibrary();
+
+const makeBook = () => {
     const title = title.value;
     const author = author.value;
     const pages = pages.value;
@@ -27,6 +40,47 @@ Book.prototype.makeBook = () => {
     return new Book(title, author, pages, isRead);
 }
 
+const toggleRead = (book) => {
+    book.isRead = !book.isRead;
+}
+
+const styleCard = (book) => {
+    const card = document.createElement('div');
+    const bookTitle = document.createElement('h2');
+    const bookAuthor = document.createElement('h2');
+    const bookPages = document.createElement('h2');
+    const bookRead = document.createElement('button');
+    const deleteBook = document.createElement('button');
+
+    card.classList.add('book-card');
+    bookRead.classList.add('toggle-read');
+    deleteBook.classList.add('delete-button');
+
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = book.author;
+    bookPages.textContent = book.pages;
+    if (book.isRead) {
+        bookRead.textContent = 'Finished!';
+    } else {
+        bookRead.textContent = 'In Progress!';
+    }
+    deleteBook.textContent= 'Delete from Library';
+
+    card.appendChild(bookTitle);
+    card.appendChild(bookAuthor);
+    card.appendChild(bookPages);
+    card.appendChild(bookRead);
+    card.appendChild(deleteBook);
+    bookDisplay.appendChild(card);
+}
+
+const displayBooks = (library) => {
+    for (let i = 0; i < library.length; i++) {
+        styleCard(library[i]);
+    }
+}
+
+displayBooks(library);
 
 
 
