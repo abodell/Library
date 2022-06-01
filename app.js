bd100173ea92785deb34f816f1cc0e0f2103a297
abodell <andybodell9@gmail.com>
2022-06-01 13:12:49 -0400

    Restarted on the javascript

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
-library = [] // array for where the books will be stored
+library = []; // where books will be stored
 
-function Book(name, author, pages, isRead) {
-    this.name = name;
+// get all elements we will need to access
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const isRead = document.querySelector('#read');
+
+function Book(title, author, pages, isRead) { // book object
+    this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
 }
 
-Book.constructor.prototype = Book; // set the prototype for constructor
+Book.constructor.prototype = Book; // setting the constructor for all Book prototypes
 
-// get all of our elements
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
-const pages = document.querySelector('#pages');
-const isRead = document.querySelector('#read');
-const addBook = document.querySelector('.button-47');
-const form = document.querySelector('.register-book');
+Book.prototype.addToLibrary = function() {
+    library.push(this);
+}
+
+Book.prototype.makeBook = () => {
+    const title = title.value;
+    const author = author.value;
+    const pages = pages.value;
+    const isRead = isRead.checked;
+    return new Book(title, author, pages, isRead);
+}
+
+
 
 
